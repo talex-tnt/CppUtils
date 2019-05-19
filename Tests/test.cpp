ea2783c3a4f31925c139c6e5c14f27e4a8375b54
Alex T <alextinti83@gmail.com>
2019-05-19 17:35:57 +0200

    + include Identifier class
    + add equality and inequality tests

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -1,6 +1,20 @@
 #include "pch.h"
+#include "../CppUtils/Identifier.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+class CarTrait{};
+using CarId = utils::Identifier<CarTrait, std::string>;
+
+
+TEST(TestEqualityOperator, TestName)
+{
+	CarId car1("MyCar");
+	CarId car2("MyCar");
+	EXPECT_TRUE(car1 == car2);
+}
+
+TEST(TestInEqualityOperator, TestName)
+{
+	CarId car1("MyCar1");
+	CarId car2("MyCar2");
+	EXPECT_TRUE(car1 != car2);
 }
