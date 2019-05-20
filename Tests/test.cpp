950f828977f842f19867941d012325630566cdc9
Alex T <alextinti83@gmail.com>
2019-05-20 17:37:44 +0200

    + add new tests

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -3,16 +3,45 @@
 
 DEFINE_IDENTIFIER(CarId, std::string);
 
-TEST(TestEqualityOperator, TestName)
+
+TEST(TestEqualityOperator, IdentifierTest)
 {
 	const CarId car1("MyCar");
 	const CarId car2("MyCar");
 	EXPECT_TRUE(car1 == car2);
 }
 
-TEST(TestInEqualityOperator, TestName)
+TEST(TestInEqualityOperator, IdentifierTest)
 {
 	const CarId car1("MyCar1");
 	const CarId car2("MyCar2");
 	EXPECT_TRUE(car1 != car2);
+}
+
+TEST(TestCopyConstructor, IdentifierTest)
+{
+	const CarId car1("MyCar");
+	const CarId car2(car1);
+	EXPECT_TRUE(car1 == car2);
+}
+
+TEST(TestCopyConstructorWithNonConstArg, IdentifierTest)
+{
+	CarId car1("MyCar");
+	const CarId car2(car1);
+	EXPECT_TRUE(car1 == car2);
+}
+
+TEST(TestCopyAssignementOperator, IdentifierTest)
+{
+	const CarId car1("MyCar");
+	const CarId car2 = car1;
+	EXPECT_TRUE(car1 == car2);
+}
+
+TEST(TestCopyAssignementOperatorWithNonConstArg, IdentifierTest)
+{
+	CarId car1("MyCar");
+	const CarId car2 = car1;
+	EXPECT_TRUE(car1 == car2);
 }
