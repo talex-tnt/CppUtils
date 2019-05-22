e7542fa2cda97bfd7751dd5fded3e6200d07b4f2
Alex T <alextinti83@gmail.com>
2019-05-22 10:04:13 +0200

    - rename tests

--- Tests/IdentifierTests.cpp
+++ Tests/IdentifierTests.cpp
@@ -4,21 +4,22 @@
 DEFINE_IDENTIFIER(CarId, std::string);
 
 
-TEST(TestConstructors, CopyConstructor)
+TEST(Identifier_TestConstructors, Identifier_CopyConstructor)
 {
+
 	const CarId car1("MyCar");
 	const CarId car2(car1);
 	EXPECT_TRUE(car1 == car2);
 }
 
-TEST(TestConstructors, CopyWithNonConstArg)
+TEST(Identifier_TestConstructors, Identifier_CopyWithNonConstArg)
 {
 	CarId car1("MyCar");
 	const CarId car2(car1);
 	EXPECT_TRUE(car1 == car2);
 }
 
-TEST(TestConstructors, MoveConstructor)
+TEST(Identifier_TestConstructors, Identifier_MoveConstructor)
 {
 	CarId car1("MyCar");
 	const CarId car2(std::move(car1));
@@ -26,35 +27,35 @@ TEST(TestConstructors, MoveConstructor)
 	EXPECT_NE(car1, CarId("MyCar"));
 }
 
-TEST(TestOperators, Equality)
+TEST(Identifier_TestOperators, Identifier_Equality)
 {
 	const CarId car1("MyCar");
 	const CarId car2("MyCar");
 	EXPECT_TRUE(car1 == car2);
 }
 
-TEST(TestOperators, InEquality)
+TEST(Identifier_TestOperators, Identifier_InEquality)
 {
 	const CarId car1("MyCar1");
 	const CarId car2("MyCar2");
 	EXPECT_TRUE(car1 != car2);
 }
 
-TEST(TestOperators, CopyAssignment)
+TEST(Identifier_TestOperators, Identifier_CopyAssignment)
 {
 	const CarId car1("MyCar");
 	const CarId car2 = car1;
 	EXPECT_TRUE(car1 == car2);
 }
 
-TEST(TestOperators, CopyAssignmentWithNonConstArg)
+TEST(Identifier_TestOperators, Identifier_CopyAssignmentWithNonConstArg)
 {
 	CarId car1("MyCar");
 	const CarId car2 = car1;
 	EXPECT_TRUE(car1 == car2);
 }
 
-TEST(TestOperators, MoveAssignment)
+TEST(Identifier_TestOperators, Identifier_MoveAssignment)
 {
 	CarId car1("MyCar1");
 	CarId car2("MyCar2");
@@ -63,19 +64,15 @@ TEST(TestOperators, MoveAssignment)
 	EXPECT_NE(car1, CarId("MyCar1"));
 }
 
-TEST(TestAccessors, GetValueLValueRef)
+TEST(Identifier_TestAccessors, Identifier_GetValueLValueRef)
 {
 	const CarId car1("MyCar");
 	EXPECT_EQ(car1.GetValue(), "MyCar");
 }
 
-TEST(TestAccessors, GetValueRValueRef)
+TEST(Identifier_TestAccessors, Identifier_GetValueRValueRef)
 {
 	const std::string c1("MyCar");
 	const std::string& c2 = CarId("MyCar").GetValue();
 	EXPECT_EQ(c1, c2);
 }
-
-
-
-
